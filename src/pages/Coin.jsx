b84c0ae4e0dae7b0953d13c566a7cc45bb68ac2e
Titus <titusonzere2@gmail.com>
2025-05-09 22:32:11 +0300

    Made all the necessary changes and finished on the graph

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -6,20 +6,26 @@ import LineChart from "../components/LineChart";
 const Coin = () => {
   const { coinId } = useParams();
   const [coinData, setCoinData] = useState();
-  const [historicalData, setHistoricalData] = useState()
+  const [historicalData, setHistoricalData] = useState();
   const { currency, setCurrency } = useStateContext();
 
-const fetchHistoricalData = async() =>{
-  const options = {
-    method: 'GET',
-    headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-QNTu6WPCJpC8tguk2qfvBGEG'}
+  const fetchHistoricalData = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-QNTu6WPCJpC8tguk2qfvBGEG",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setHistoricalData(res))
+      .catch((err) => console.error(err));
   };
-  
-  fetch(`https://api.coingecko.com/api/v3/coins/${coinId}/market_chart?vs_currency=${currency.name}&days=10&interval=daily`, options)
-    .then(res => res.json())
-    .then(res => setHistoricalData(res))
-    .catch(err => console.error(err));
-}
 
   const fetchCoinData = async () => {
     const options = {
@@ -37,26 +43,84 @@ const fetchHistoricalData = async() =>{
   };
   useEffect(() => {
     fetchCoinData();
-    fetchHistoricalData()
+    fetchHistoricalData();
     console.log(coinData);
   }, [currency]);
 
-  if (coinData, historicalData) {
+  if ((coinData, historicalData)) {
     return (
       // coin
-      <div>
+      <div className="px-0 py-5">
         {/* coin name */}
-        <div>
-          <img src={coinData.image.large} alt="" />
-          <p>
+        <div className="flex flex-col items-center gap-5 mx-[100px] my-auto mb-12.5 ">
+          <img src={coinData.image.large} alt="" className="max-w-[100px]  "/>
+          <p className="text-[44px] font-bold">
             <b>
               {coinData.name} ({coinData.symbol.toUpperCase()}){" "}
             </b>
           </p>
         </div>{" "}
+        <div className="max-w-[600px] h-[250px] m-auto ">
+          <LineChart historicalData={historicalData} />
+        </div>
+        {/* coin info */}
+        <div className="max-w-[600px] my-[50px] mx-auto flex flex-col">
+          <ul className="flex justify-between px-2.5 py-0 border-b border-[#5f5f5f] ">
+            <li>Crypto Market Rank</li>
+            <li>{coinData.market_cap_rank}</li>
+          </ul>
 
-        <div>
-          <LineChart historicalData={historicalData}/>
+           <ul className="flex justify-between px-2.5 py-0 border-b border-[#5f5f5f] ">
+            <li>Current Rank</li>
+            {currency &&
+              coinData?.market_data?.current_price[currency.name] && (
+                <li>
+                  {currency.symbol}{" "}
+                  {coinData.market_data.current_price[
+                    currency.name
+                  ].toLocaleString()}
+                </li>
+              )}
+          </ul>
+
+           <ul className="flex justify-between px-2.5 py-0 border-b border-[#5f5f5f] ">
+            <li>Market Cap</li>
+            {currency &&
+              coinData?.market_data?.current_price[currency.name] && (
+                <li>
+                  {currency.symbol}{" "}
+                  {coinData.market_data.market_cap[
+                    currency.name
+                  ].toLocaleString()}
+                </li>
+              )}
+          </ul>
+
+           <ul className="flex justify-between px-2.5 py-0 border-b border-[#5f5f5f] ">
+            <li>24 Hour high</li>
+            {currency &&
+              coinData?.market_data?.current_price[currency.name] && (
+                <li>
+                  {currency.symbol}{" "}
+                  {coinData.market_data.high_24h[
+                    currency.name
+                  ].toLocaleString()}
+                </li>
+              )}
+          </ul>
+
+          <ul className="font-semibold flex justify-between px-2.5 py-0 border-b border-[#5f5f5f]">
+            <li>24 Hour low</li>
+            {currency &&
+              coinData?.market_data?.current_price[currency.name] && (
+                <li>
+                  {currency.symbol}{" "}
+                  {coinData.market_data.low_24h[
+                    currency.name
+                  ].toLocaleString()}
+                </li>
+              )}
+          </ul>
         </div>
       </div>
     );
